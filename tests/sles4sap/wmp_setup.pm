30819c3f6d5c342061b1fbaa55a41dad85eab212
Ednilson Miura <emiura@suse.com>
2023-03-15 10:22:46 -0300

    fixup

--- tests/sles4sap/wmp_setup.pm
+++ tests/sles4sap/wmp_setup.pm
@@ -21,7 +21,7 @@ sub run {
 
     # WMP is a feature of SLES for SAP Applications 15+. Skip test in older
     # systems and sle15sp5+ (PED-131)
-    if (is_sle('<15') || (is_sle('15-SP5+')))
+    if (is_sle('<15') || (is_sle('15-SP5+'))){
         record_info 'WMP', 'WMP is only available in SLES for SAP Applications 15+';
         return;
     }
