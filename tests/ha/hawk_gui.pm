0206cecbc6d3311b5fce1444b25e856533b5781e
Ednilson Miura <emiura@suse.com>
2023-03-22 10:16:17 -0300

    bla

--- tests/ha/hawk_gui.pm
+++ tests/ha/hawk_gui.pm
@@ -58,7 +58,7 @@ sub run {
     #assert_script_run("docker pull $docker_image", 240);
     assert_script_run "curl -s  $docker_file";
     assert_script_run "docker load -i hawk_test.tar";
-    assert_script_run "docker run 8f7a0bf06b48";
+    #assert_script_run "docker run 8f7a0bf06b48";
 
     # Rest of the test needs to be performed on the x11 console, but with the
     # HA_CLUSTER setting that console is not yet activated; newer versions of gdm
@@ -88,7 +88,8 @@ sub run {
     assert_script_run "xhost +";
     barrier_wait("HAWK_GUI_CPU_TEST_START_$cluster_name");
     my $docker_cmd = "docker run --rm --name test --ipc=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=\$DISPLAY -v \$PWD/$path:/$path ";
-    $docker_cmd .= "$docker_image -b $browser -H $node1 -S $node2 -s $testapi::password -r /$results --virtual-ip $virtual_ip";
+    $docker_cmd .= "8f7a0bf06b48 -b $browser -H $node1 -S $node2 -s $testapi::password -r /$results --virtual-ip $virtual_ip";
+    #$docker_cmd .= "$docker_image -b $browser -H $node1 -S $node2 -s $testapi::password -r /$results --virtual-ip $virtual_ip";
     enter_cmd "$docker_cmd 2>&1 | tee $logs; echo $pyscr-\$PIPESTATUS > $retcode";
     assert_screen "hawk-$browser", 60;
 
